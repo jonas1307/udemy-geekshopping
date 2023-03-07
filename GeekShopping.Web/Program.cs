296e8cf4d7e0cdac98b9c9cd9a2aa9635035a292
Jonas Lima de Amorim <dev.amorim@outlook.com>
2023-03-07 10:42:55 -0300

    feat: implementing httpclientextensions

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,16 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient<IProductService, ProductService>(x =>
+{
+    x.BaseAddress = new Uri(configuration["ServiceUrls:ProductApi"]);
+});
 
 var app = builder.Build();
 
---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue _contentType { get; set; } = new MediaTypeHeaderValue("application/json");
+
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static async Task<HttpResponseMessage> PostAsJson<T>(this HttpClient client, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+
+            var content = new StringContent(dataAsString);
+
+            content.Headers.ContentType = _contentType;
+
+            return await client.PostAsync(url, content);
+        }
+
+        public static async Task<HttpResponseMessage> PutAsJson<T>(this HttpClient client, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+
+            var content = new StringContent(dataAsString);
+
+            content.Headers.ContentType = _contentType;
+
+            return await client.PutAsync(url, content);
+        }
+    }
+}
