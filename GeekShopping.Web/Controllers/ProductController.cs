d97f36002838d442eecbfc82a37802485ff47f63
Jonas Lima de Amorim <dev.amorim@outlook.com>
2023-03-07 18:36:30 -0300

    feat: implementing product actions

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await _productService.FindAllProducts();
+
+            return View(products);
+        }
+
+        public IActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+
+                if (response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(long id)
+        {
+            var model = await _productService.GetProductById(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+
+                if (response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(long id)
+        {
+            var model = await _productService.GetProductById(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            var response = await _productService.DeleteProductById(model.Id);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+        }
+    }
+}
